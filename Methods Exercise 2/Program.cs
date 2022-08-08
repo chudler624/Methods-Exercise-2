0b6aecd4e78f1f553934eb4f7ed1725dda862ff5
Casey Hudler <chudler624@gmail.com>
2022-08-08 12:14:10 -0400

    initial commit

---
+++ Methods Exercise 2/Program.cs
@@ -0,0 +1,28 @@
+﻿static int Add(int x, int y)
+{
+ return   (x + y);
+
+  
+}
+Console.WriteLine(Add(1, 3));
+
+
+static int Multiply(int x, int y)
+{
+    return (x * y);
+}
+Console.WriteLine(Multiply(4, 5));
+
+
+static int Divide(int x, int y)
+{
+    return (x / y);
+}
+Console.WriteLine(Divide(72, 8));
+
+
+static int Subtract(int x, int y)
+{
+    return (x - y);
+}
+Console.WriteLine(Subtract(10, 7));
